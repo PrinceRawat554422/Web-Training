a9ea81276aef2a9b49507e3b484dfcd4f37b9a45
PrinceRawat554422 <princerawat22445@gmail.com>
2024-12-02 15:55:46 +0530

    complete javascript concepts day6

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("-----Welcome in JS----")
+
+console.log("----variables in JS-----");
+
+// var, let, const :
+
+var var1 = 5;
+var var2 = "variable-2-in string-firmat";
+var var3 = 345134835154;
+var var4 = [2 ,3 ,4 56, 7, "hello"];
+
+console.log(var1, var2, var3);
+
+
+
+
+
+
+
+
+
+
+
+//print sum out of 5 numbers:
+//ptint average of numbers :
+//Greater number out of num1, num2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Loops:
+for (let in = 0; i < 10; i++) {
+    const tabline =   
+    console.log(newValue);
+}
