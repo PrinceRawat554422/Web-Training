937f0d41cfe5df3392d3766f724b2f0013fc71fe
PrinceRawat554422 <princerawat22445@gmail.com>
2024-12-03 16:02:20 +0530

    day 7

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-console.log("-----Welcome in JS----")
+console.log("----welcome in js----");
+// const title =  document.getElementById("title");
+// console.log(title);
 
-console.log("----variables in JS-----");
-
-// var, let, const :
-
-var var1 = 5;
-var var2 = "variable-2-in string-firmat";
-var var3 = 345134835154;
-var var4 = [2 ,3 ,4 56, 7, "hello"];
-
-console.log(var1, var2, var3);
+// title.innerText = "hii i'm ......";
 
 
+// const desc = document.getElementsByClassName("desc");
+// console.log(desc);
 
+// desc[0].innerText = "hey bro...";
 
+const divEle = document.getElementsByClassName("heading");
+// divEle[2].innerText = "First World";
+for(let i = 0; i < divEle.length; i++) {
+if(i % 2 === 0){
+    divEle[i].innerText = "even";
+}
+else{
+    divEle[i].innerText = "odd";
+}
+}
+// divEle[3].innerText = "Second World";
+// console.log(heading);
 
+//select
+// const h1Ele =  document.getElementById("heading");
 
+//new content varible:
+// const newContent = "-----12345----";
 
-
-
-
-
-//print sum out of 5 numbers:
-//ptint average of numbers :
-//Greater number out of num1, num2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Loops:
-for (let in = 0; i < 10; i++) {
-    const tabline =   
-    console.log(newValue);
+// Assign Value to Tag:
+// h1Ele.innerText = newContent;
+const Arr = [10,9,8,7,6,5,4,3,2,1]
+for(let i=0; i<Arr.length; i++){
+    console.log(Arr[i]);
+}
+// for(let i =0;i < Arr.length; i++){
+//     if(i % 2 ===0){
+//         console.log(i+"="+Arr[i]+"even");
+// }
+// else{
+//     console.log(i+"="+Arr[i]+"odd");
+// }
+// }
+for(let i=0; i<Arr.length; i++){
+    console.log(add(Arr.length))
 }
