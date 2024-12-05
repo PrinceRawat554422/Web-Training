c60f97cd1997a9f3abe4bc25ace31dc6b680f9b2
PrinceRawat554422 <princerawat22445@gmail.com>
2024-12-05 15:59:32 +0530

    content(function and function calling)
    day 9 (functions, background folder design

--- index.js
+++ index.js
@@ -1,78 +1,164 @@
-// console.log("-----Wecome in JS-------");
-// let arr = [1,2,3,4,5,6,7,8, "hello",true];
-// for(let i = 0; i<arr.length;i++){
-// console.log(arr[i]);
-// }
+console.log("-----Wecome in JS-------");
+let A = [9845];
+;
+console.log(A);
 
-// arr.push("prince");
-// console.log(".............");
-// arr.pop();
-// // arr.splice(start,count,addon)
-// arr.splice(2,1);
-// for(let i=0;i<arr.length; i++){
-//     console.log(arr[i]);
-// }
-// const newarr = [5,5,6,4,3,5,4,56,4,3];
-// let count = 0;
-// for(let i=0; i< arr.length; i++) {
-//     console.log(arr[i])
-//     if(arr[i] === 5) count++;
-//     if(arr[i] % 2 === 0)count++;
-// }
-// console.log(count);
-// let arr2 = [1,4,5,6,73,2,4];
-// let sum = 0;
-// for(let i = 0; i<arr2.length;i++){
-//     console.log(arr2[i]);
-//     if(arr2[i] === 5) {
-//         arr2.splice(i,1);
-// }
 
-// let arr3 = [1,2,3,4,5,7,8,9];
-// let missing = 0;
-// for(let i = 0; i<arr3.length;i++){
-//      console.log(arr3[i]);
-//     if(arr3[i] === i) {
-//        arr3.missing(i)
-//     }
+
+// deep sir repo .json
+// const data = [
+//     {
+//        "image": {
+//             "thumbnail": "./assets/images/image-waffle-thumbnail.jpg",
+//             "mobile": "./assets/images/image-waffle-mobile.jpg",
+//             "tablet": "./assets/images/image-waffle-tablet.jpg",
+//             "desktop": "./assets/images/image-waffle-desktop.jpg"
+//        },
+//        "name": "Waffle with Berries",
+//        "category": "Waffle",
+//        "price": 6.50
+//     },
+//     {
+//         "image": {
+//             "thumbnail": "./assets/images/image-creme-brulee-thumbnail.jpg",
+//             "mobile": "./assets/images/image-creme-brulee-mobile.jpg",
+//             "tablet": "./assets/images/image-creme-brulee-tablet.jpg",
+//             "desktop": "./assets/images/image-creme-brulee-desktop.jpg"
+//         },
+//         "name": "Vanilla Bean Crème Brûlée",
+//         "category": "Crème Brûlée",
+//         "price": 7.00
+//      },
+//      {
+//         "image": {
+//             "thumbnail": "./assets/images/image-macaron-thumbnail.jpg",
+//             "mobile": "./assets/images/image-macaron-mobile.jpg",
+//             "tablet": "./assets/images/image-macaron-tablet.jpg",
+//             "desktop": "./assets/images/image-macaron-desktop.jpg"
+//         },
+//         "name": "Macaron Mix of Five",
+//         "category": "Macaron",
+//         "price": 8.00
+//      },
+//      {
+//         "image": {
+//             "thumbnail": "./assets/images/image-tiramisu-thumbnail.jpg",
+//             "mobile": "./assets/images/image-tiramisu-mobile.jpg",
+//             "tablet": "./assets/images/image-tiramisu-tablet.jpg",
+//             "desktop": "./assets/images/image-tiramisu-desktop.jpg"
+//         },
+//         "name": "Classic Tiramisu",
+//         "category": "Tiramisu",
+//         "price": 5.50
+//      },
+//      {
+//         "image": {
+//             "thumbnail": "./assets/images/image-baklava-thumbnail.jpg",
+//             "mobile": "./assets/images/image-baklava-mobile.jpg",
+//             "tablet": "./assets/images/image-baklava-tablet.jpg",
+//             "desktop": "./assets/images/image-baklava-desktop.jpg"
+//         },
+//         "name": "Pistachio Baklava",
+//         "category": "Baklava",
+//         "price": 4.00
+//      },
+//      {
+//         "image": {
+//             "thumbnail": "./assets/images/image-meringue-thumbnail.jpg",
+//             "mobile": "./assets/images/image-meringue-mobile.jpg",
+//             "tablet": "./assets/images/image-meringue-tablet.jpg",
+//             "desktop": "./assets/images/image-meringue-desktop.jpg"
+//         },
+//         "name": "Lemon Meringue Pie",
+//         "category": "Pie",
+//         "price": 5.00
+//      },
+//      {
+//         "image": {
+//             "thumbnail": "./assets/images/image-cake-thumbnail.jpg",
+//             "mobile": "./assets/images/image-cake-mobile.jpg",
+//             "tablet": "./assets/images/image-cake-tablet.jpg",
+//             "desktop": "./assets/images/image-cake-desktop.jpg"
+//         },
+//         "name": "Red Velvet Cake",
+//         "category": "Cake",
+//         "price": 4.50
+//      },
+//      {
+//         "image": {
+//             "thumbnail": "./assets/images/image-brownie-thumbnail.jpg",
+//             "mobile": "./assets/images/image-brownie-mobile.jpg",
+//             "tablet": "./assets/images/image-brownie-tablet.jpg",
+//             "desktop": "./assets/images/image-brownie-desktop.jpg"
+//         },
+//         "name": "Salted Caramel Brownie",
+//         "category": "Brownie",
+//         "price": 4.50
+//      },
+//      {
+//         "image": {
+//             "thumbnail": "./assets/images/image-panna-cotta-thumbnail.jpg",
+//             "mobile": "./assets/images/image-panna-cotta-mobile.jpg",
+//             "tablet": "./assets/images/image-panna-cotta-tablet.jpg",
+//             "desktop": "./assets/images/image-panna-cotta-desktop.jpg"
+//         },
+//         "name": "Vanilla Panna Cotta",
+//         "category": "Panna Cotta",
+//         "price": 6.50
+//      }
+// ];
+// const ele = document.getElementById("data")
+// // let i =0;
+// for(let i = 0;i<data.length;i++){
+//     const name1 = data[i].name; 
+//     const price = data[i].price;
+//     const category = data[i].category;
+//     console.log("......",name1, category, price);
+    
+   
+//     // const htmldata1 = `<h1> ${price} </h1>`;
+//     // const htmldata = ;
+//     ele.innerHTML += `
+    
+//     <div class="card">
+//     <h1>${name1}</h1>
+//     <h2>${price}</h2>
+//     <h3>${category}</h3>
+// </div>`
+// ;
+
+// const htmldata = <div class="card">
+
+
+// ele.innerHTML += htmldata;
+
+// ele.innerHTML += htmldata;
+
+    // console.log(name1);
+    // console.log(price);
+    // console.log(category);
 // }
+// const firstValue = data[0].name;
+// const firstValue1 = data[1].category;
+// const price = data[1].price;
 
-let student_id_card = {
-    name: "rohan",
-    rollno : 2222,
-    address : 'abc...',
-    section: 'C',
-    course : "B-Tech",
-    BRANCH : "CSE",
-    session : "2024 -2025"
+// console.log("first value", firstValue);
+// console.log("first value", firstValue1);
+// console.log("price=", price);
+// console.log("name", name);
 
+
+// -------Functions-------;
+// types of functions: - predefined, userdefined
+// Named funtion, arrow function, Iffi only we will do here.
+
+// Named Function
+function sum(a,b){
+    const sum = a+b;
+    console.log(sum);
 }
-console.log(student_id_card);
-const name = student_id_card.name;
-const  rollno = student_id_card.rollno;
-const address = student_id_card.address;
-const section = student_id_card.section;
-const course = student_id_card.course;
-const BRANCH = student_id_card.BRANCH;
-const session = student_id_card.session;
-// print :
 
-console.log(name);
-console.log(rollno);
-console.log(address);
-console.log(section);
-console.log(course);
-console.log(BRANCH);
-console.log(session);
-// add new key and value:
-
-student_id_card.result = "pass";
-student_id_card.id = "12";
-// update :
-student_id_card.section = "B";
-// delete :
-delete student_id_card.section;
+sum(2,5);
 
 
-console.log(student_id_card);
 
