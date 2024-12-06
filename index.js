5a5774b963e147942b2e30ab067fdd39a61c40d7
PrinceRawat554422 <princerawat22445@gmail.com>
2024-12-06 15:59:32 +0530

    day 10 code complete(try catch,fetch api)

--- index.js
+++ index.js
@@ -1,164 +1,53 @@
-console.log("-----Wecome in JS-------");
-let A = [9845];
-;
-console.log(A);
 
+// function sum(a,b){
+//     for(let i=0;i<20;i++){
+//         sum(0+i)
+//         console.log(sum);
+//     }
 
-
-// deep sir repo .json
-// const data = [
-//     {
-//        "image": {
-//             "thumbnail": "./assets/images/image-waffle-thumbnail.jpg",
-//             "mobile": "./assets/images/image-waffle-mobile.jpg",
-//             "tablet": "./assets/images/image-waffle-tablet.jpg",
-//             "desktop": "./assets/images/image-waffle-desktop.jpg"
-//        },
-//        "name": "Waffle with Berries",
-//        "category": "Waffle",
-//        "price": 6.50
-//     },
-//     {
-//         "image": {
-//             "thumbnail": "./assets/images/image-creme-brulee-thumbnail.jpg",
-//             "mobile": "./assets/images/image-creme-brulee-mobile.jpg",
-//             "tablet": "./assets/images/image-creme-brulee-tablet.jpg",
-//             "desktop": "./assets/images/image-creme-brulee-desktop.jpg"
-//         },
-//         "name": "Vanilla Bean Crème Brûlée",
-//         "category": "Crème Brûlée",
-//         "price": 7.00
-//      },
-//      {
-//         "image": {
-//             "thumbnail": "./assets/images/image-macaron-thumbnail.jpg",
-//             "mobile": "./assets/images/image-macaron-mobile.jpg",
-//             "tablet": "./assets/images/image-macaron-tablet.jpg",
-//             "desktop": "./assets/images/image-macaron-desktop.jpg"
-//         },
-//         "name": "Macaron Mix of Five",
-//         "category": "Macaron",
-//         "price": 8.00
-//      },
-//      {
-//         "image": {
-//             "thumbnail": "./assets/images/image-tiramisu-thumbnail.jpg",
-//             "mobile": "./assets/images/image-tiramisu-mobile.jpg",
-//             "tablet": "./assets/images/image-tiramisu-tablet.jpg",
-//             "desktop": "./assets/images/image-tiramisu-desktop.jpg"
-//         },
-//         "name": "Classic Tiramisu",
-//         "category": "Tiramisu",
-//         "price": 5.50
-//      },
-//      {
-//         "image": {
-//             "thumbnail": "./assets/images/image-baklava-thumbnail.jpg",
-//             "mobile": "./assets/images/image-baklava-mobile.jpg",
-//             "tablet": "./assets/images/image-baklava-tablet.jpg",
-//             "desktop": "./assets/images/image-baklava-desktop.jpg"
-//         },
-//         "name": "Pistachio Baklava",
-//         "category": "Baklava",
-//         "price": 4.00
-//      },
-//      {
-//         "image": {
-//             "thumbnail": "./assets/images/image-meringue-thumbnail.jpg",
-//             "mobile": "./assets/images/image-meringue-mobile.jpg",
-//             "tablet": "./assets/images/image-meringue-tablet.jpg",
-//             "desktop": "./assets/images/image-meringue-desktop.jpg"
-//         },
-//         "name": "Lemon Meringue Pie",
-//         "category": "Pie",
-//         "price": 5.00
-//      },
-//      {
-//         "image": {
-//             "thumbnail": "./assets/images/image-cake-thumbnail.jpg",
-//             "mobile": "./assets/images/image-cake-mobile.jpg",
-//             "tablet": "./assets/images/image-cake-tablet.jpg",
-//             "desktop": "./assets/images/image-cake-desktop.jpg"
-//         },
-//         "name": "Red Velvet Cake",
-//         "category": "Cake",
-//         "price": 4.50
-//      },
-//      {
-//         "image": {
-//             "thumbnail": "./assets/images/image-brownie-thumbnail.jpg",
-//             "mobile": "./assets/images/image-brownie-mobile.jpg",
-//             "tablet": "./assets/images/image-brownie-tablet.jpg",
-//             "desktop": "./assets/images/image-brownie-desktop.jpg"
-//         },
-//         "name": "Salted Caramel Brownie",
-//         "category": "Brownie",
-//         "price": 4.50
-//      },
-//      {
-//         "image": {
-//             "thumbnail": "./assets/images/image-panna-cotta-thumbnail.jpg",
-//             "mobile": "./assets/images/image-panna-cotta-mobile.jpg",
-//             "tablet": "./assets/images/image-panna-cotta-tablet.jpg",
-//             "desktop": "./assets/images/image-panna-cotta-desktop.jpg"
-//         },
-//         "name": "Vanilla Panna Cotta",
-//         "category": "Panna Cotta",
-//         "price": 6.50
-//      }
-// ];
-// const ele = document.getElementById("data")
-// // let i =0;
-// for(let i = 0;i<data.length;i++){
-//     const name1 = data[i].name; 
-//     const price = data[i].price;
-//     const category = data[i].category;
-//     console.log("......",name1, category, price);
-    
-   
-//     // const htmldata1 = `<h1> ${price} </h1>`;
-//     // const htmldata = ;
-//     ele.innerHTML += `
-    
-//     <div class="card">
-//     <h1>${name1}</h1>
-//     <h2>${price}</h2>
-//     <h3>${category}</h3>
-// </div>`
-// ;
-
-// const htmldata = <div class="card">
-
-
-// ele.innerHTML += htmldata;
-
-// ele.innerHTML += htmldata;
-
-    // console.log(name1);
-    // console.log(price);
-    // console.log(category);
+    // const sum = a+b;
+    // console.log(sum);
 // }
-// const firstValue = data[0].name;
-// const firstValue1 = data[1].category;
-// const price = data[1].price;
-
-// console.log("first value", firstValue);
-// console.log("first value", firstValue1);
-// console.log("price=", price);
-// console.log("name", name);
-
-
-// -------Functions-------;
-// types of functions: - predefined, userdefined
-// Named funtion, arrow function, Iffi only we will do here.
-
-// Named Function
-function sum(a,b){
-    const sum = a+b;
-    console.log(sum);
+// const sum = (a+b) => {
+//     console.log(a+b)
+// }
+const counter = document.getElementById("counter");
+let count = 0;
+function incrementValue(){
+    count++;
+    counter.innerText = count;
+}
+function decrementValue(){
+    count--;
+    counter.innerText = count;
 }
 
-sum(2,5);
+// const a = 5;
+// const b = 7;
+// console.log("value1:",a)
+// try{
+// a = 6;
+// console.log("value2:",b);
+// }catch(error){
+//     console.log(error);
+// }
 
+// console.log("value3:",b);
+// function myPromiseFun(res,rej){}
+// const myPromise = new Promise(myPromiseFun);
 
+// console.log(myPromise);
 
+const myPromise = new Promise(solve);
+function solve(fun1,fun2){
+    let n=9999;
+    while(n--);
+    fun1("Data");
+}
+console.log("------bro-----");
+console.log(myPromise);
+console.log("------yes------");
+
+// H.W
+// setTimeout
+// setInterval
